9e26482cccfbf1d08b9ea8c6fa8a063b18be4746
reneesch <renee.scheffler@gmx.com>
2022-01-18 19:53:46 +0100

    ID und Interesse an Mathematik
    ID angelegt und Versuch, einen Vektor fuer Interesse an Mathe in Zusammenhang mit Studienfach zu erzeugen.

--- Datensatz.R
+++ Datensatz.R
@@ -11,3 +11,28 @@ Faecher <- c("Statistik", "Data Science", "Mathe", "Informatik") # Vektor der ST
 Studienfach <- sample(Faecher, size = 100, replace = TRUE, prob = c(0.3,0.3,0.1,0.2))
 ## Studienfaecher zufaellig ausgewahlt aus dem Vektor, wobei Statistik & Data Science Wkeit von 0.3 haben,
 ## Mathe von 0.1 und Informatik von 0.2
+
+## Interesse an Mathematik simulieren
+
+Interesse_Mathe0 <- sample(1:7, size = 100, replace = TRUE) ## zunaechst Vektor aus diskreter Gleichvert. erzeugen
+## dabei noch kein Zusammenhang mit Studienfach
+Interesse_Mathe_Zushang <- function(x){ ## Funktion, um das Interesse an Mathematik mit dem Studienfach in 
+  ## Zusammenhang zu bringen
+  ifelse(Studienfach == "Mathe", x + 2, x) # bei Studienfach Mathe
+  ## 2 Punkte addieren
+  ifelse(Studienfach == "Statistik"| 
+           Studienfach == "Data Science", x + 1, x)
+  ## bei Studienfach Statistik oder Data Science 1 Punkt addieren, sonst gleich lassen
+}
+Interesse_Mathe1 <- Interesse_Mathe_Zushang(Interesse_Mathe0) # Funktion aufrufen, um Zusammenhang
+## zu Studienfach zu erzeugen
+abrunden <- function(x){## Funktion zum Abrunden: falls Addition zu Punktzahl hoeher als 7 
+  ##gefuehrt hat, Wert fuer Interesse auf 7 reduzieren
+  ifelse(x > 7, x == 7, x)
+}
+Interesse_Mathe <- abrunden(Interesse_Mathe1) ## abrunden durchfuehren
+
+any(Interesse_Mathe > 7) ## Kontrolle, ob Rundung korrekt durchgefuehrt wurde
+
+## ID für ID-Spalte anlegen
+ID <- c(1:100)

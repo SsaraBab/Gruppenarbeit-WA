bbeef49cb6abb1162d3f7accdadcb1ab28bc644c
Moshirimozafar <93552347+Moshirimozafar@users.noreply.github.com>
2022-02-23 00:05:12 +0100

    R-Skript4 wurde von mir implementiert und R-Skript1 und Datensatz.csv benutzt bzw. aufgerufen.

--- R-Skript 4 - (Aufgabe 4).R
+++ R-Skript 4 - (Aufgabe 4).R
@@ -1,7 +1,34 @@
 ## R-Skript 4
+
 #  Hier sollte mit Hilfe der in (3.) in Funktionen-R-Skript 1 erstellten Funktionen den 
 #  Datensatz aus (2.) analysiert werden (Deskription und Visualisierung). Hierbei Sollte
 #  dies R-Skript in Minimum jede der Funktionen (a) bis (f) aus Funktionen-R-Skript 1 
 #  einmal anwenden.
 
 
+# Import Dateien
+
+# Install R Packages "Tidyverse"
+
+library(tidyverse)
+
+library(readr)
+
+Datensatz <- read_csv("Datensatz.csv")
+
+# Nutzen von Dateien
+
+dplyr::glimpse(Datensatz)
+
+# Implementierung der Funktionen
+
+## (Funktion a) Eine Funktion, die verschiedene geeignete deskriptive Statistiken fuer metrische
+## Variablen berechnet und ausgibt
+
+# R-Skript1 einzulesen
+source("Funktionen-R-Skript1.R") 
+
+# Funktion a aufzurufen
+function(metrisch)
+x <- Datensatz$ID
+metrisch(x)

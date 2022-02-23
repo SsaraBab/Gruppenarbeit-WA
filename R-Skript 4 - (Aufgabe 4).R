c71c1cc6043d2317c33885b9590ed092fb21d4e0
Moshirimozafar <93552347+Moshirimozafar@users.noreply.github.com>
2022-02-23 22:32:41 +0100

    Implementierung R-Skript4  mit Hilfe R-Skript1
    Das wird aktualisieren...

--- R-Skript 4 - (Aufgabe 4).R
+++ R-Skript 4 - (Aufgabe 4).R
@@ -32,3 +32,44 @@ source("Funktionen-R-Skript1.R")
 function(metrisch)
 x <- Datensatz$ID
 metrisch(x)
+
+## (Funktion b) Eine Funktion, die verschiedene geeignete deskriptive Statistiken 
+## fuer kategoriale Variablen berechnet und ausgibt
+
+
+
+
+## (Funktion c) Eine Funktion, die geeignete deskriptive bivariate Statistiken fuer
+## den Zusammenhang zwischen zwei kategorialen Variablen
+## berechnet ausgibt
+
+
+
+
+## (Funktion d) Eine Funktion, die geeignete deskriptive bivariate Statistiken fuer
+## den Zusammengang zwischen einer metrischen und einer
+## dichotomen Variablen berechnet und ausgibt
+function(metrisch.dichotom)
+m <- c(Datensatz$Interesse_Programmieren)
+l <- 
+  
+metrisch.dichotom(l, m)
+
+
+## (Funktion e) Eine Funktion, die eine mindestens ordinal skalierte Variable
+## quantilbasiert kategorisiert (z.B. in "niedrig", "mittel", "hoch")
+function (create.quantil)
+q <- Datensatz$Interesse_Programmieren
+create.quantil(q)
+create.quantil(q,0,1/2)
+
+
+
+## (Funktion f) Eine Funktion, die eine geeignete Visualisierung von drei oder vier
+## kategorialen Variablen erstellt.
+
+# Funktion a aufzurufen
+function(visualisierung)
+  
+data <- data.frame ()
+visualisierung(data)
